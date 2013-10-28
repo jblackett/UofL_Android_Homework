ef43be7ad0ef36f3895bf53cc9d8b9001592b42e
Joseph Blackett <joseph.blackett@gmail.com>
2013-10-28 16:40:35 -0400

    updated to book status

--- homework5/CrimeActivity/src/com/jblackett/criminalintent/Crime.java
+++ homework5/CrimeActivity/src/com/jblackett/criminalintent/Crime.java
@@ -1,51 +1,46 @@
 package com.jblackett.criminalintent;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
-	
-	private UUID mID;
-	private String mTitle;
-	private Date mDate;
-	private boolean mSolved;
-	
-		
-	public Crime(){
-		//Generate unique identifier
-		mID = UUID.randomUUID();
-		mDate = new Date(0);
-	}
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
 
-	public String getTitle() {
-		return mTitle;
-	}
+    public String getTitle() {
+        return mTitle;
+    }
 
-	public void setTitle(String title) {
-		mTitle = title;
-	}
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 
-	public UUID getID() {
-		return mID;
-	}
+    public UUID getId() {
+        return mId;
+    }
 
-	public Date getDate() {
+	public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+    
+    public Date getDate() {
 		return mDate;
 	}
 
 	public void setDate(Date date) {
-		date = mDate;
+		mDate = date;
 	}
 
-	public boolean isSolved() {
-		return mSolved;
-	}
-
-	public void setSolved(boolean solved) {
-		solved = mSolved;
-	}
-	
-	
-	
-	
+    
 }
--- homework5/CrimeActivity/src/com/jblackett/criminalintent/CrimeActivity.java
+++ homework5/CrimeActivity/src/com/jblackett/criminalintent/CrimeActivity.java
@@ -1,34 +1,26 @@
 package com.jblackett.criminalintent;
 
-//change shit for git
-//more changes
-//change shit again for git
-
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-        
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        
-        android.support.v4.app.Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-        
-        if (fragment == null){
-        	fragment = new CrimeFragment();
-        	fm.beginTransaction()
-        		.add(R.id.fragmentContainer, fragment)
-        		.commit();
-        }
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        } 
     }
-
-
-
 }
+
