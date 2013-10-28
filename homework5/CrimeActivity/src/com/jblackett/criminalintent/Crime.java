cf22d09deff9ecd00b3a28e1a6699b1f065eb3d8
Joseph Blackett <joseph.blackett@gmail.com>
2013-10-28 16:17:34 -0400

    8.1 and 8.2

--- homework5/CrimeActivity/src/com/jblackett/criminalintent/Crime.java
+++ homework5/CrimeActivity/src/com/jblackett/criminalintent/Crime.java
@@ -1,27 +1,51 @@
 package com.jblackett.criminalintent;
 
+import java.sql.Date;
 import java.util.UUID;
 
 public class Crime {
 	
 	private UUID mID;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	
+		
 	public Crime(){
 		//Generate unique identifier
 		mID = UUID.randomUUID();
+		mDate = new Date(0);
 	}
 
-	public String getmTitle() {
+	public String getTitle() {
 		return mTitle;
 	}
 
-	public void setmTitle(String title) {
+	public void setTitle(String title) {
 		mTitle = title;
 	}
 
-	public UUID getmID() {
+	public UUID getID() {
 		return mID;
 	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		date = mDate;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		solved = mSolved;
+	}
+	
+	
+	
 	
 }
