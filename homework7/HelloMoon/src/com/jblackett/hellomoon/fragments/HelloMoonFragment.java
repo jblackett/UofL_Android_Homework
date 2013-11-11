51d51e5cc38821cfe645caec17d969fbdcc40bf8
Joseph Blackett <joseph.blackett@gmail.com>
2013-11-11 16:17:18 -0500

    audio Player

--- homework7/HelloMoon/src/com/jblackett/hellomoon/fragments/HelloMoonFragment.java
+++ homework7/HelloMoon/src/com/jblackett/hellomoon/fragments/HelloMoonFragment.java
@@ -1,6 +1,8 @@
 package com.jblackett.hellomoon.fragments;
 
-import android.R;
+
+import com.jblackett.hellomoon.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
